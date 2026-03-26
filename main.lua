7650d80f8d8b6ebc584a3a76473b03a9ef53bc1c
Haider <muhammedhaider982@gmail.com>
2026-03-26 16:42:48 +0400

    Enhance installation flow.
    Enhance showError (it was empty) and add getDiskDrive to enhance the installation. This also changes the argument you need for install from "install" to "installToDrive" (ONLY does as the name suggests).

--- main.lua
+++ main.lua
@@ -3,13 +3,32 @@
     Falcon SE main program. This is the entry point for the Falcon SE environment.
 --]]
 
-local function showError()
-    
+-- Shows an error and end sthe program.
+local function showError(error_code, additional_info)
+    print("An error has occured.")
+    print("Error code: " .. error_code)
+    print("Additional info: " .. additional_info)
+    return
 end
 
-local function enforeInstallRequirements()
+local function getDiskDrive()
     local disk_drive = peripheral.find("drive")
-    
+
+    -- Disk drive check
+    while not disk_drive do
+        print("Disk drive not found! Please attach a disk drive!")
+        sleep(1)
+        disk_drive = peripheral.find("drive")
+    end
+    print("Disk found! Continuing with installation.")
+
+    -- Disk check
+    while not disk_drive.isDiskPresent() do
+        print("Disk not found!")
+        sleep(1)
+    end
+    print("Disk found!")
+
     return disk_drive
 end
 
@@ -25,8 +44,32 @@ if args[1] == "recover" then
     os.reboot()
 end
 
-if args[1] == "install" then
-    local installurl = args[2]
+if args[1] == "installToDrive" then
+    local installUrl = args[2]
+    local fileName
+    local diskDrive = getDiskDrive()
 
+    -- Check for HTTP
+    if not http then
+        showError("NO_HTTP", "Turn on HTTP for your world.")
+    end
+
+    -- Send a request
+    local request = http.get(installUrl)
+    local code = request.readAll()
+    request.close()
+
+    -- Write to the file.
+    local file = fs.open(diskDrive.getMountPath() .. fileName, "w")
     
+    -- Check if fs.open returns nil
+    if not file then
+        showError("FILE_ERROR", "File cannot be opened. Make sure the disk is not read only and there is space on disk.")
+    end
+
+    fs.write(code)
+    fs.close()
+
+    diskDrive.ejectDisk()
+    print("Done installing! You may now use your program.")
 end
