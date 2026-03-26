f2fa27e5bf5f1b2bcc5d5c9dcece8a2dcf682783
Haider <muhammedhaider982@gmail.com>
2026-03-26 14:10:43 +0400

    Add some basic structure
    Add the showError() function (does nothing for now) and adds the enforeInstallRequirements() function (doesnt actually enforce them, only returns disk drive.). This also adds checking if the first argument is "install" and gets the installurl from the second argument.

--- main.lua
+++ main.lua
@@ -3,9 +3,18 @@
     Falcon SE main program. This is the entry point for the Falcon SE environment.
 --]]
 
+local function showError()
+    
+end
+
+local function enforeInstallRequirements()
+    local disk_drive = peripheral.find("drive")
+    
+    return disk_drive
+end
+
 local args = {...}
 print("Welcome to the Falcon special environment.")
-
 
 if args[1] == "recover" then
     print("Falcon SE is starting in recovery mode...")
@@ -14,4 +23,10 @@ if args[1] == "recover" then
     print("Computer will reboot in 3 seconds.")
     sleep(3)
     os.reboot()
+end
+
+if args[1] == "install" then
+    local installurl = args[2]
+
+    
 end
